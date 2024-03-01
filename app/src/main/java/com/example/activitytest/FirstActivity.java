c37f910897979788d76ffd5a001b01151d106f11
Stephen <1446806199@qq.com>
2024-03-02 00:26:25 +0800

    [chore]Button Toast Menu

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -1,13 +1,44 @@
 package com.example.activitytest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add_item) {
+            Toast.makeText(this, "click Add", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == R.id.remove_item) {
+            Toast.makeText(this, "click Remove", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "You click Button 1", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
     }
 }
