adad49f2085049676633137e8224ad0f737659c9
Stephen <1446806199@qq.com>
2024-03-03 21:35:27 +0800

    [chore]Intent相关

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -1,9 +1,13 @@
 package com.example.activitytest;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -36,9 +40,33 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FirstActivity.this, "You click Button 1", Toast.LENGTH_SHORT).show();
-                finish();
+                Toast.makeText(FirstActivity.this, "You click Button 1, intent to 2", Toast.LENGTH_SHORT).show();
+//                finish();
+//                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+//                Intent intent = new Intent("com.example.activitytest.ACTION_START");
+//                intent.addCategory("com.example.activitytest.MY_CATEGORY");
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("http://www.baidu.com"));
+                String data = "Hello Second Activity";
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("extra_data", data);
+                startActivityForResult(intent, 1);
             }
         });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity:", returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
---
+++ app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -0,0 +1,43 @@
+package com.example.activitytest;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("extra_data");
+        Log.d("SecondActivity:", data);
+
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "Hello FirstActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed(); 需要把super注释掉或者后置，默认会设置setResult(RESULT_CANCELED)
+        Log.d("SecondActivity:", "onBackPressed");
+        Intent intent = new Intent();
+        intent.putExtra("data_return", "Hello FirstActivity back pressed");
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
